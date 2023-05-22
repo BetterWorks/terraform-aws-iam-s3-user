66344485c5df0c8f47fdc97359060ca76443ca40
Henry He <henry.he@betterworks.com>
2023-05-22 14:28:01 -0600

    fix: removing terraform 1 tags and branches

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ data "aws_iam_policy_document" "default" {
 
     content {
       sid = "AllBucketPermissions"
-      actions   = [
-                    "s3:ListAllMyBuckets",
-                    "s3:GetBucketLocation"
-                  ]
+      actions = [
+        "s3:ListAllMyBuckets",
+        "s3:GetBucketLocation"
+      ]
       resources = ["arn:aws:s3:::*"]
       effect    = "Allow"
     }
@@ -24,7 +24,7 @@ data "aws_iam_policy_document" "default" {
 
 
 module "s3_user" {
-  source        = "git::https://github.com/betterworks/terraform-aws-iam-system-user.git?ref=tags/1.0.0-terraform-1"
+  source        = "git::https://github.com/betterworks/terraform-aws-iam-system-user.git?ref=tags/0.6.0"
   namespace     = var.namespace
   stage         = var.stage
   name          = var.name
